c06247ecc4befb56060fcf937bc2e7e60fcbe804
adm_user2001 <yuquilemajoel@gmail.com>
2024-01-09 21:52:39 -0500

    Inicializar proyecto

---
+++ src/main/java/com/example/puce/ProyectoTramitesApplication.java
@@ -0,0 +1,13 @@
+package com.example.puce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTramitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTramitesApplication.class, args);
+	}
+
+}
